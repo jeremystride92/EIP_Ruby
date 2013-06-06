29f05ae6f112c3c5044932144413c57634690b3c
John Tuley <john@quickleft.com>
2013-06-06 09:44:40 -0600

    [#50014155] Fixes from code review

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,7 +3,7 @@ class Admin::BaseController < ApplicationController
   before_filter :namespace_view_path
 
   private
-  #
+
   # Allow namespaced template inheritance in application directory
   def namespace_view_path
     prepend_view_path 'app/views/admin'
