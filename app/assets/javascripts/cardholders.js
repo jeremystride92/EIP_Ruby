39d8a7864a4071ef7dd2400b56c918aa8a23da91
Garrett Motzner & John Tuley <garrett+john@quickleft.com>
2013-06-13 16:32:02 -0600

    [#50014961] Card detail view with visibilty toggle

--- app/assets/javascripts/cardholders.js
+++ app/assets/javascripts/cardholders.js
@@ -1,3 +1,12 @@
 $('#card_level_id').change(function() {
   $('#card_level_form').submit();
 });
+
+$('.accordion-header').click(function(e) {
+  var header = $(e.currentTarget);
+  header.toggleClass('info');
+  header.siblings('.accordion-header').removeClass('info');
+  var body = header.next('.accordion-body');
+  body.siblings('.accordion-body').addClass('hidden');
+  body.toggleClass('hidden');
+});
