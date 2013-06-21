877c406e87f0215b59e74ecf47f4c03cf3456fe2
John Tuley <john@tuley.org>
2013-06-21 11:13:03 -0500

    Reformat API::V1::CardholdersController

--- app/controllers/api/v1/cardholders_controller.rb
+++ app/controllers/api/v1/cardholders_controller.rb
@@ -1,18 +1,21 @@
 class Api::V1::CardholdersController < ApplicationController
   before_filter :authorize
+
   def show
   end
 
   private
-    def authorize
-      if cardholder = authenticate_with_http_token { |token, options| Cardholder.find_by_auth_token(token) }
-        @cardholder = cardholder
+  
+  def authorize
+
+    if cardholder = authenticate_with_http_token { |token, options| Cardholder.find_by_auth_token(token) }
+      @cardholder = cardholder
+    else
+      if Rails.env.development? && params[:phone].present?
+        @cardholder = Cardholder.find_by_phone_number(params[:phone])
       else
-        if Rails.env.development? && params[:phone].present?
-          @cardholder = Cardholder.find_by_phone_number(params[:phone])
-        else
-          request_http_token_authentication
-        end
+        request_http_token_authentication
       end
     end
+  end
 end
