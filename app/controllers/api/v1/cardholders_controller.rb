72489db7292b6b63136e5390c05b832ace54ce68
John Tuley <john@quickleft.com>
2013-06-21 14:57:05 -0600

    [#52064029] Remove developer backdoor

--- app/controllers/api/v1/cardholders_controller.rb
+++ app/controllers/api/v1/cardholders_controller.rb
@@ -11,11 +11,7 @@ class Api::V1::CardholdersController < ApplicationController
     if cardholder = authenticate_with_http_token { |token, options| Cardholder.find_by_auth_token(token) }
       @cardholder = cardholder
     else
-      if Rails.env.development? && params[:phone].present?
-        @cardholder = Cardholder.find_by_phone_number(params[:phone])
-      else
-        request_http_token_authentication
-      end
+      request_http_token_authentication
     end
   end
 end
