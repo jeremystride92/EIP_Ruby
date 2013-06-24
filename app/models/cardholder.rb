45cee3329932ff47a0bded174069eb40e66d8958
John Tuley <john@quickleft.com>
2013-06-24 13:47:29 -0600

    [#50015279] Add Cardholder status to simplify logic around name presence validation

--- app/models/cardholder.rb
+++ app/models/cardholder.rb
@@ -1,4 +1,6 @@
 class Cardholder < ActiveRecord::Base
+  STATUSES = %w(pending active)
+
   has_secure_password
 
   has_many :cards, order: :status
@@ -6,8 +8,10 @@ class Cardholder < ActiveRecord::Base
 
   has_many :venues, through: :cards
 
-  validates :first_name, presence: true, on: :update
-  validates :last_name, presence: true, on: :update
+  validates :first_name, presence: true, unless: :pending?
+  validates :last_name, presence: true, unless: :pending?
+
+
   validates :password, length: { in: 6..255 }, allow_nil: true
   validates :phone_number,
     presence: true,
@@ -18,11 +22,27 @@ class Cardholder < ActiveRecord::Base
   before_create { generate_token(:auth_token) }
   before_validation :generate_unusable_password!, unless: proc { |cardholder| cardholder.password_digest||cardholder.password }
 
+  before_validation :set_default_status
+
   def generate_unusable_password!
     self.password = self.password_confirmation = SecureRandom.random_bytes(16)
   end
 
+  STATUSES.each do |status_code|
+    define_method("#{status_code}?") do
+      self.status == status_code
+    end
+  end
+
+  def activate!
+    update_attributes(status: 'active')
+  end
+
   private
+
+  def set_default_status
+    self.status ||= 'pending'
+  end
 
   def generate_token(column)
     begin
