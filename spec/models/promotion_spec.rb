323991b264b8a82d2e1863864d705743c624d979
John Tuley <john@tuley.org>
2013-07-01 14:36:45 -0600

    Correct whitespace in promotion_spec.rb

--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 
 describe Promotion do
   describe "associations" do
-    it { should belong_to(:venue) }
+    it { should belong_to :venue  }
   end
+
   describe "validations" do
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:venue) }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :venue }
   end
+
   describe 'factory' do
     subject { create :promotion }
+
     it { should be_valid }
   end
 end
