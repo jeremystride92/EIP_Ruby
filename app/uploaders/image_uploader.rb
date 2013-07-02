12755000170a2d3ef5bea5f5495fbadffe0b9ce1
Garrett Motzner <garrett@quickleft.com>
2013-07-02 09:30:57 -0600

    [#50014435] Changes per pull request Remove unused uploader version Change date output for promotions list to better match comps.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -42,10 +42,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [128, 128]
   end
 
-  version :list_thumbnail do
-    process resize_to_fit: [200, 200]
-  end
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_white_list
