5cc66637cc12af601478ecbff69eb14548f118bf
Garrett Motzner <garrett@quickleft.com>
2013-07-08 13:27:39 -0600

    Add default image to other uploaders.

--- app/uploaders/cardholder_image_uploader.rb
+++ app/uploaders/cardholder_image_uploader.rb
@@ -2,7 +2,7 @@ class CardholderImageUploader < ImageUploader
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
-    asset_path("fallback/logo-placeholder.png")
+    asset_path("fallback/photo-placeholder.png")
     #"/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,6 +13,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :fog
 
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    asset_path("fallback/image-placeholder.png")
+    #"/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
