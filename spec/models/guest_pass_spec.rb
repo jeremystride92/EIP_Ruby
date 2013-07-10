83f98314994036c243ee8fd9da6628dcaecb324e
Garrett Motzner <garrett@quickleft.com>
2013-07-10 15:53:35 -0600

    [#53164963] Use TimeWithZone everywhere

--- spec/models/guest_pass_spec.rb
+++ spec/models/guest_pass_spec.rb
@@ -14,7 +14,7 @@ describe GuestPass do
       end
 
       it "should clear datetime field if empty start_date_field" do
-        time = Time.now
+        time = Time.zone.now
         b = create :guest_pass, start_date: time, start_date_field: '', start_time_field: '11:22 PM'
         b.start_date.should be_nil
       end
@@ -28,7 +28,7 @@ describe GuestPass do
       end
 
       it "should clear datetime field if empty end_date_field" do
-        time = Time.now
+        time = Time.zone.now
         b = create :guest_pass, end_date: time, end_date_field: '', end_time_field: '11:22 PM'
         b.end_date.should be_nil
       end
