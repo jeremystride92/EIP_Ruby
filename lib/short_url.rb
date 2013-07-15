8dc854d94da90a3d79975ca522bd551af61eb620
Garrett Motzner <garrett@quickleft.com>
2013-07-15 13:41:41 -0600

    [#53254147] Add simple Caching wrapper to bitly

---
+++ lib/short_url.rb
@@ -0,0 +1,31 @@
+class ShortUrl
+  def self.shorten(url, skip_cache: false, **options)
+    @cache ||= {}
+    if skip_cache
+      Bitly.client.shorten(url, options)
+    else
+      search_cache(url) || add_to_cache(url, Bitly.client.shorten(url, options))
+    end
+  end
+
+  def self.cache
+    @cache ||= {}
+  end
+
+  def self.cache=(cache)
+    @cache = cache
+  end
+
+
+  private
+
+  def self.search_cache(url)
+    @cache[url]
+  end
+
+  def self.add_to_cache(url, short_url)
+    @cache[url] = short_url
+  end
+
+
+end
---
+++ spec/lib/short_url_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe ShortUrl do
+  subject {ShortUrl}
+
+  let(:client){ double('client') }
+
+  before do
+    Bitly.stub(:client).and_return(client)
+  end
+
+  it "should call bitly if url not cached" do
+    subject.cache.clear
+    client.should_receive(:shorten).with(any_args()).and_return("http://sho.rt/1")
+    subject.shorten('http://shouldnotbecached.com')
+  end
+
+  it "should call bitly if url cached AND skip_cache is true" do
+    subject.cache['http://iamchached.com'] = "http://sho.rt/1"
+    client.should_receive(:shorten).with(any_args()).and_return("http://sho.rt/1")
+    subject.shorten('http://iamchached.com', skip_cache: true)
+  end
+
+  it "should not call bitly if cache has url" do
+    subject.cache['http://iamchached.com'] = "http://sho.rt/1"
+    client.should_not_receive(:shorten)
+    subject.shorten('http://iamchached.com')
+  end
+end
