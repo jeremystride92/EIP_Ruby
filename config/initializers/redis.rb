feee86ac2898e160028813ea388c4e858722e4b1
Garrett <garrett@quickleft.com>
2013-07-16 17:38:56 -0600

    Revert change to redis initializer to fix circle tests

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
-uri = URI.parse(ENV['REDISTOGO_URL'])
-$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+if ENV['REDISTOGO_URL'].blank?
+  $redis = Redis.new(host: 'localhost', port: 6379)
+else
+  uri = URI.parse(ENV['REDISTOGO_URL'])
+  $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+end
