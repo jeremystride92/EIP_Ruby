d152a921af9a7a44260b9ac250c5cf01acbc4fbc
John Tuley <john@quickleft.com>
2013-07-17 17:04:22 -0600

    [#53551613] Authorize via CanCan and add Benefits to nav

--- app/controllers/benefits_controller.rb
+++ app/controllers/benefits_controller.rb
@@ -1,11 +1,10 @@
 class BenefitsController < ApplicationController
   before_filter :authenticate
   before_filter :find_venue
-  before_filter :find_benefits
-
-  skip_authorization_check
 
   def index
+    authorize! :read, Benefit
+
     tz = ActiveSupport::TimeZone[@venue.time_zone]
     @start_date = params[:start_date] ? tz.parse(params[:start_date]).beginning_of_day : tz.now.beginning_of_day
     @end_date = params[:end_date] ? tz.parse(params[:end_date]).end_of_day : tz.now.end_of_day
@@ -18,8 +17,4 @@ class BenefitsController < ApplicationController
     @venue = Venue.includes(cards: [:benefits, :cardholder]).find(current_user.venue_id)
     @cards = @venue.cards
   end
-
-  def find_benefits
-  end
-
 end
