eff2bef41447dfbc9ae31996b2caa8a514e8fb2b
Garrett Motzner <garrett@quickleft.com>
2013-07-18 17:04:57 -0600

    [#53550335] Make underscore/dash style consistent

--- app/assets/javascripts/cardholders_batch_new.js
+++ app/assets/javascripts/cardholders_batch_new.js
@@ -1,30 +1,30 @@
 //= require cardholder_batch_fields
 
 (function() {
-  var fields_template = JST['cardholder_batch_fields'];
+  var fieldsTemplate = JST['cardholder_batch_fields'];
 
-  $('.batch_cardholders').delegate('#issue_another', 'click', function(e) {
+  $('.batch-cardholders').delegate('#issue-another', 'click', function(e) {
     e.preventDefault();
 
     var $button = $(e.currentTarget);
     var index = $button.data('index');
-    var $batch_area = $('.batch_cardholders .batch_rows');
+    var $batchArea = $('.batch-cardholders .batch-rows');
 
     var nextIndex = new Date().getTime();
-    $batch_area.append(fields_template({ index: nextIndex }));
+    $batchArea.append(fieldsTemplate({ index: nextIndex }));
     $('#cardholders_' + (nextIndex) + '_phone_number').focus();
   });
 
-  $('.batch_cardholders .batch_rows').delegate('.button-remove', 'click', function(e) {
+  $('.batch-cardholders .batch-rows').delegate('.button-remove', 'click', function(e) {
     e.preventDefault();
     var $button = $(e.currentTarget);
     var index = $button.data('index');
-    var $fieldset = $('.cardholder_fields[data-index="' + index + '"]');
+    var $fieldset = $('.cardholder-fields[data-index="' + index + '"]');
 
     $fieldset.remove();
   });
 
   $(function() {
-    $('.batch_cardholders .batch_rows').append(fields_template({ index: 0 }));
+    $('.batch-cardholders .batch-rows').append(fieldsTemplate({ index: 0 }));
   });
 })();
