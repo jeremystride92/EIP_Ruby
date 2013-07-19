9bfff08a9f684c91c2fca6f2130dbb8188c45a27
John Tuley <john@quickleft.com>
2013-07-19 11:02:31 -0600

    [#53552105] Issue temporary benefits to Card Level

--- app/controllers/card_levels_controller.rb
+++ app/controllers/card_levels_controller.rb
@@ -4,6 +4,7 @@ class CardLevelsController < ApplicationController
   before_filter :find_venue
   before_filter :find_venue_card_levels, only: [:index]
   before_filter :find_card_level, only: [:edit, :update, :reorder]
+  before_filter :find_card_level_with_benefits, only: [:issue_benefits_form, :issue_benefits]
 
   def index
     authorize! :read, CardLevel if @card_levels.empty?
@@ -39,7 +40,6 @@ class CardLevelsController < ApplicationController
   def update
     authorize! :update, @card_level
 
-    card_level_attributes = card_level_params
     if @card_level.update_attributes(card_level_params)
       redirect_to venue_card_levels_path, notice: 'Card level updated.'
     else
@@ -53,6 +53,21 @@ class CardLevelsController < ApplicationController
     @card_level.reorder_to params[:sort_position].to_i
 
     redirect_to venue_card_levels_path
+  end
+
+  def issue_benefits_form
+    authorize! :update, @card_level
+  end
+
+  def issue_benefits
+    binding.pry
+    authorize! :update, @card_level
+
+    if @card_level.update_attributes(temp_benefit_params)
+      redirect_to venue_card_levels_path, notice: 'Temporary benefits updated.'
+    else
+      render :issue_benefits_form
+    end
   end
 
   private
@@ -69,7 +84,15 @@ class CardLevelsController < ApplicationController
     @card_level = CardLevel.where(venue_id: @venue).find(params[:id] || params[:card_level_id])
   end
 
+  def find_card_level_with_benefits
+    @card_level = CardLevel.includes(:benefits).where(venue_id: @venue).find(params[:card_level_id])
+  end
+
   def card_level_params
-    params.require(:card_level).permit(:name, :theme, :daily_guest_pass_count, benefits_attributes: [:id, :description, :_destroy])
+    params.require(:card_level).permit(:name, :theme, :daily_guest_pass_count, permanent_benefits_attributes: [:id, :description, :_destroy])
+  end
+
+  def temp_benefit_params
+    params.require(:card_level).permit(temp_benefits_attributes: [:id, :description, :start_date_field, :start_time_field, :end_date_field, :end_time_field, :_destroy])
   end
 end
