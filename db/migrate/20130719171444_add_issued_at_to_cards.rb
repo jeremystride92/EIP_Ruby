9be2f973c22b0d1c93e23c2754b2a5d5f33cbefb
Garrett Motzner <garrett@quickleft.com>
2013-07-19 18:07:45 -0600

    Remove data manipulations from migrations
    Code to run on server is below.
    
    ```ruby
    Cardholder.all.each do |cardholder|
        cardholder.activated_at = cardholder.updated_at if cardholder.active?
        cardholder.save
    end
    
    Card.reset_column_information
    Card.all.each do |card|
      card.issued_at = card.updated_at unless card.pending?
      card.save
    end
    
    ```

--- db/migrate/20130719171444_add_issued_at_to_cards.rb
+++ db/migrate/20130719171444_add_issued_at_to_cards.rb
@@ -1,14 +1,4 @@
 class AddIssuedAtToCards < ActiveRecord::Migration
-  def migrate(direction)
-    super
-    if direction == :up
-      Card.reset_column_information
-      Card.all.each do |card|
-        card.issued_at = card.updated_at unless card.pending?
-        card.save
-      end
-    end
-  end
   def change
     add_column :cards, :issued_at, :datetime
   end
