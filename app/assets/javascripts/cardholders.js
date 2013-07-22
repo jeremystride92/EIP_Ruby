5bea863e284e637afdc3af7adad950f65076321d
Garrett Motzner <garrett@quickleft.com>
2013-07-22 17:02:59 -0600

    [#53571403] Fix bug on Cardholders#index where changing card level did not update header row.

--- app/assets/javascripts/cardholders.js
+++ app/assets/javascripts/cardholders.js
@@ -28,8 +28,7 @@ $('form.change-card-level').bind("ajax:success", function(xhr, data) {
   $body.removeClass('warning').addClass('success');
   $header.removeClass('warning');
 
-  $header.find('.card-level').text(data.card_level.name);
-  $body.find('.card-level').text(data.card_level.name);
+  $header.find('.card-level-name').text(data.card_level.name);
   $body.find('.card-preview').removeClass(function(index, classes) {
         return (classes.match(/\btheme-\S+/g) || []).join(' ');
       }).addClass('theme-' + data.card_level.theme);
