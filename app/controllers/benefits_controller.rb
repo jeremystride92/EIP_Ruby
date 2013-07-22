ae18a982e51810509deee174a0020bb8e137d0a1
John Tuley <john@quickleft.com>
2013-07-22 17:56:38 -0600

    [#53551613] Break out benefits list by day

--- app/controllers/benefits_controller.rb
+++ app/controllers/benefits_controller.rb
@@ -5,12 +5,21 @@ class BenefitsController < ApplicationController
   def index
     authorize! :read, Benefit
 
-    @cards.reject! { |card| card.benefits.empty? }
-
     tz = ActiveSupport::TimeZone[@venue.time_zone]
-    @start_date = params[:start_date] ? tz.parse(params[:start_date]).beginning_of_day : tz.now.beginning_of_day
-    @end_date = params[:end_date] ? tz.parse(params[:end_date]).end_of_day : tz.now.end_of_day
-    @end_date = @start_date.end_of_day if @end_date < @start_date
+    @start_date = params[:start_date] ? tz.parse(params[:start_date]).to_date : tz.now.to_date
+    @end_date = params[:end_date] ? tz.parse(params[:end_date]).to_date : tz.now.to_date
+    @end_date = @start_date if @end_date < @start_date
+
+    @benefits_by_date = {}
+    (@start_date..@end_date).each do |date|
+      @benefits_by_date[date] = []
+      @cards.each do |card|
+        active_benefits = Time.use_zone(tz) { card.benefits.select { |benefit| benefit.active_in_range? date.beginning_of_day, date.end_of_day } }
+        if active_benefits.present?
+          @benefits_by_date[date] << { cardholder_name: card.cardholder.display_name, active_benefits: active_benefits }
+        end
+      end
+    end
   end
 
   private
