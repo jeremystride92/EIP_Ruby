de7aad92fb95bea3f4462ccd4c2e0aa4dcdcbe6c
Garrett Motzner <garrett@quickleft.com>
2013-07-24 17:10:25 -0600

    [#53970187] Redirect admin users to admin page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_authorization_check
 
   def index
+    redirect_to admin_root_path and return if current_user && current_user.has_role?(:site_admin)
     redirect_to venue_path if current_user
   end
 end
