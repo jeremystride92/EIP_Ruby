230a1eb499b13701de3540b297721e7274bf1113
Garrett Motzner <garrett@quickleft.com>
2013-07-24 17:43:56 -0600

    [#53031307] Crop cardholder photos to a square

--- app/uploaders/cardholder_image_uploader.rb
+++ app/uploaders/cardholder_image_uploader.rb
@@ -6,6 +6,9 @@ class CardholderImageUploader < ImageUploader
     #"/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
+  # process all files
+  process :crop_square
+
   # Create different versions of your uploaded files:
   version :mobile_small do
     process resize_to_fit: [150, 150]
@@ -13,5 +16,18 @@ class CardholderImageUploader < ImageUploader
 
   version :mobile_large do
     process resize_to_fit: [300, 300]
+  end
+
+  def crop_square
+    manipulate! do |image|
+      if image[:width] < image[:height]
+        remove = ((image[:height] - image[:width])/2).round
+        image.shave("0x#{remove}")
+      elsif image[:width] > image[:height]
+        remove = ((image[:width] - image[:height])/2).round
+        image.shave("#{remove}x0")
+      end
+      image
+    end
   end
 end
