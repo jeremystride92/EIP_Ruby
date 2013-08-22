0f4df44eb2403ea301362d5d5b42eaec60f55cf5
Benjamin West <mrgenixus@gmail.com>
2013-08-22 10:49:43 -0600

    Fixed Typos and Semantic issues

--- app/mailers/sms_mailer.rb
+++ app/mailers/sms_mailer.rb
@@ -3,9 +3,9 @@ class SmsMailer < ActionMailer::Base
 
   def cardholder_onboarding_sms(cardholder_id, venue_id)
     @cardholder = Cardholder.find(cardholder_id);
-    raise "Cardholder not found: #{cardholder_id}" if @cardholder.nil?
+    raise "Cardholder not found: #{cardholder_id}" unless @cardholder.present?
     @venue = Venue.find(venue_id)
-    raise "Venue not found: #{venue_id}" if @venue.nil?
+    raise "Venue not found: #{venue_id}" unless @venue.present?
 
     mail to: ENV['site_email'] # Needed to activate message (see https://github.com/rails/rails/pull/8048)
     self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: venue.sender_number, message: render_to_string)
@@ -13,8 +13,8 @@ class SmsMailer < ActionMailer::Base
 
   def cardholder_new_card_sms(cardholder_id, venue_id)
     @cardholder = Cardholder.find(cardholder_id)
-    @venue = Venuf.find(venue_id)
-    raise "Cardholder not found: #{cardholder_id}" if @cardholder.nil?
+    @venue = Venue.find(venue_id)
+    raise "Cardholder not found: #{cardholder_id}" unless @cardholder.present?
     raise "Venue not found: #{venue_id}" if @venue.nil?
 
     mail to: ENV['site_email'] # Needed to activate message
@@ -23,13 +23,13 @@ class SmsMailer < ActionMailer::Base
 
   def cardholder_promotion_message(cardholder_id, venue_id, message)
 
-    @cardholder = Cardholder.find(cardholder_id)
-    @venue = Venuf.find(venue_id)
-    raise "Cardholder not found: #{cardholder_id}" if @cardholder.nil?
+    cardholder = Cardholder.find(cardholder_id)
+    @venue = Venue.find(venue_id)
+    raise "Cardholder not found: #{cardholder_id}" unless cardholder.present?
     raise "Venue not found: #{venue_id}" if @venue.nil?
 
     mail to: ENV['site_email']
-    self.message.delivery_handler = NexmoSender.new(to: @cardholder.international_phone_number, from: @venue.sender_number, message: message)
+    self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: @venue.sender_number, message: message)
   end
 
   def temp_card_sms(card_id, venue_id, partner_id)
@@ -37,9 +37,9 @@ class SmsMailer < ActionMailer::Base
     @venue = Venue.find(venue_id)
     @partner = Partner.find(partner_id)
 
-    raise "Cardholder not found: #{cardholder_id}" if @cardholder.nil?
-    raise "Venue not found: #{venue_id}" if @venue.nil?
-    raise "Partner not found: #{partner_id}" if @partner.nil?
+    raise "Cardholder not found: #{cardholder_id}" unless @cardholder.present?
+    raise "Venue not found: #{venue_id}" unless @venue.present?
+    raise "Partner not found: #{partner_id}" unless @partner.present?
 
     @link = $short_url_cache.shorten public_temporary_card_url(@card.access_token, subdomain: @venue.vanity_slug), skip_cache: true
 
@@ -48,11 +48,11 @@ class SmsMailer < ActionMailer::Base
   end
 
   def pin_reset_sms(cardholder_id)
-    @cardholder = Cardholder.find(cardholder_id)
-    raise "Cardholder not found: #{cardholder_id}" if @cardholder.nil?
+    cardholder = Cardholder.find(cardholder_id)
+    raise "Cardholder not found: #{cardholder_id}" unless cardholder.present?
 
     @token = @cardholder.reset_token
-    @phone_number = c@ardholder.phone_number
+    @phone_number = cardholder.phone_number
 
     raise "Venue not found for cardholder: #{cardholder_id}" unless cardholder.venues.count
     @venue = @cardholder.venues.first
