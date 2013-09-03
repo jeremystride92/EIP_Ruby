c27c3a77581a036436639b68c626761975f39a82
John Tuley <john@quickleft.com>
2013-09-03 14:10:27 -0600

    Remove unhit raise commands

--- app/mailers/sms_mailer.rb
+++ app/mailers/sms_mailer.rb
@@ -2,10 +2,8 @@ class SmsMailer < ActionMailer::Base
   default from: ENV['site_email']
 
   def cardholder_onboarding_sms(cardholder_id, venue_id)
-    @cardholder = Cardholder.find(cardholder_id);
-    raise "Cardholder not found: #{cardholder_id}" unless @cardholder.present?
+    @cardholder = Cardholder.find(cardholder_id)
     @venue = Venue.find(venue_id)
-    raise "Venue not found: #{venue_id}" unless @venue.present?
 
     mail to: ENV['site_email'] # Needed to activate message (see https://github.com/rails/rails/pull/8048)
     self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: venue.sender_number, message: render_to_string)
@@ -14,8 +12,6 @@ class SmsMailer < ActionMailer::Base
   def cardholder_new_card_sms(cardholder_id, venue_id)
     @cardholder = Cardholder.find(cardholder_id)
     @venue = Venue.find(venue_id)
-    raise "Cardholder not found: #{cardholder_id}" unless @cardholder.present?
-    raise "Venue not found: #{venue_id}" if @venue.nil?
 
     mail to: ENV['site_email'] # Needed to activate message
     self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: venue.sender_number, message: render_to_string)
@@ -25,8 +21,6 @@ class SmsMailer < ActionMailer::Base
 
     cardholder = Cardholder.find(cardholder_id)
     @venue = Venue.find(venue_id)
-    raise "Cardholder not found: #{cardholder_id}" unless cardholder.present?
-    raise "Venue not found: #{venue_id}" if @venue.nil?
 
     mail to: ENV['site_email']
     self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: @venue.sender_number, message: message)
@@ -37,10 +31,6 @@ class SmsMailer < ActionMailer::Base
     @venue = Venue.find(venue_id)
     @partner = Partner.find(partner_id)
 
-    raise "Cardholder not found: #{cardholder_id}" unless @cardholder.present?
-    raise "Venue not found: #{venue_id}" unless @venue.present?
-    raise "Partner not found: #{partner_id}" unless @partner.present?
-
     @link = $short_url_cache.shorten public_temporary_card_url(@card.access_token, subdomain: @venue.vanity_slug), skip_cache: true
 
     mail to: ENV['site_email']
@@ -49,12 +39,10 @@ class SmsMailer < ActionMailer::Base
 
   def pin_reset_sms(cardholder_id)
     cardholder = Cardholder.find(cardholder_id)
-    raise "Cardholder not found: #{cardholder_id}" unless cardholder.present?
 
     @token = cardholder.reset_token
     @phone_number = cardholder.phone_number
 
-    raise "Venue not found for cardholder: #{cardholder_id}" unless cardholder.venues.count
     @venue = cardholder.venues.first
 
     @link = $short_url_cache.shorten reset_pin_cardholder_url(@token)
