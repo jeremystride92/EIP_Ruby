11699b5a162840c5d7222709acc9c61038795ad4
John Tuley <john@quickleft.com>
2013-09-06 10:15:44 -0600

    [#53840317] Add benefit/guest pass handlers to SmsMailer

--- app/mailers/sms_mailer.rb
+++ app/mailers/sms_mailer.rb
@@ -6,7 +6,7 @@ class SmsMailer < ActionMailer::Base
     @venue = Venue.find(venue_id)
 
     mail to: ENV['site_email'] # Needed to activate message (see https://github.com/rails/rails/pull/8048)
-    self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: venue.sender_number, message: render_to_string)
+    self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: @venue.sender_number, message: render_to_string)
   end
 
   def cardholder_new_card_sms(cardholder_id, venue_id)
@@ -14,7 +14,7 @@ class SmsMailer < ActionMailer::Base
     @venue = Venue.find(venue_id)
 
     mail to: ENV['site_email'] # Needed to activate message
-    self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: venue.sender_number, message: render_to_string)
+    self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: @venue.sender_number, message: render_to_string)
   end
 
   def cardholder_promotion_message(cardholder_id, venue_id, message)
@@ -34,18 +34,35 @@ class SmsMailer < ActionMailer::Base
     @link = $short_url_cache.shorten public_temporary_card_url(@card.access_token, subdomain: @venue.vanity_slug), skip_cache: true
 
     mail to: ENV['site_email']
-    self.message.delivery_handler = NexmoSender.new(to: card.international_phone_number, from: venue.sender_number, message: render_to_string)
+    self.message.delivery_handler = NexmoSender.new(to: card.international_phone_number, from: @venue.sender_number, message: render_to_string)
   end
 
   def pin_reset_sms(cardholder_id)
     cardholder = Cardholder.find(cardholder_id)
 
     @token = cardholder.reset_token
-    @phone_number = cardholder.phone_number
-
     @venue = cardholder.venues.first
-
     @link = $short_url_cache.shorten reset_pin_cardholder_url(@token)
+
+    mail to: ENV['site_email']
+    self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: @venue.sender_number, message: render_to_string)
+  end
+
+  def cardholder_new_benefit_sms(cardholder_id, venue_id, count)
+    cardholder = Cardholder.find cardholder_id
+    @venue = Venue.find venue_id
+
+    @count = count
+
+    mail to: ENV['site_email']
+    self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: @venue.sender_number, message: render_to_string)
+  end
+
+  def cardholder_new_guest_pass_sms(cardholder_id, venue_id, count)
+    cardholder = Cardholder.find cardholder_id
+    @venue = Venue.find venue_id
+
+    @count = count
 
     mail to: ENV['site_email']
     self.message.delivery_handler = NexmoSender.new(to: cardholder.international_phone_number, from: @venue.sender_number, message: render_to_string)
