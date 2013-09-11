8fa41a11efdf38c003427c59398d21c97dce284b
John Tuley <john@quickleft.com>
2013-09-11 13:40:31 -0600

    [#56708878] Use card theme for temporary cards

--- app/assets/javascripts/temporary_card.js
+++ app/assets/javascripts/temporary_card.js
@@ -4,25 +4,35 @@ function hideAddressBar() {
   }, 0);
 }
 
+function setBackgroundImage(orientation) {
+  $('.card-container').css('background-image', 'url(' + themeBackgrounds[orientation] + ')');
+}
+
 $(function() {
   window.addEventListener("orientationchange", function() {
     switch(window.orientation) {
       case 0:
       case 180:
         $('.card-container').removeClass('landscape').addClass('portrait');
+        setBackgroundImage('portrait');
         break;
       case 90:
       case -90:
         $('.card-container').addClass('landscape').removeClass('portrait');
+        setBackgroundImage('landscape');
         hideAddressBar();
         break;
     };
+
+
   }, false);
 
   if (window.orientation % 180) {
     $('.card-container').addClass('landscape');
+    setBackgroundImage('landscape');
     hideAddressBar();
   } else {
     $('.card-container').addClass('portrait');
+    setBackgroundImage('portrait');
   }
 });
