8fd6d64b1ef41e0fd28d8920d57e4168e1dfb7c4
Benjamin West <ben@quickleft.com>
2013-11-20 15:03:51 -0700

    Changed Button Text, Fixed Image Scaling

--- app/assets/javascripts/cardholder_onboarding.js
+++ app/assets/javascripts/cardholder_onboarding.js
@@ -1,16 +1,17 @@
-function confirmRetake(e){
-  var $this = $(e.currentTarget);
-
-  if ($this.is('[class*=retake]')){
-    $(self).click();
-  } else {
-    var $elements = $(self).parents('form').find('input,textarea,button,select').not('[type="hidden"]')
-    $elements.get($elements.index(self) + 1).focus();
-  }
-}
-
 $('#cardholder_photo').change(function() {
   var self = this;
+
+  function confirmRetake(e){
+    var $this = $(e.currentTarget);
+
+    if ($this.is('[class*=retake]')){
+      $(self).click();
+    } else {
+      var $elements = $(self).parents('form').find('input,textarea,button,select').not('[type="hidden"]')
+      $elements.get($elements.index(self) + 1).focus();
+    }
+  }
+
   readImagePreviewToBackground(this, $('.photo-preview'));
 
   $('#PhotoReminderModal').modal().one('click','.btn',confirmRetake);
@@ -33,3 +34,15 @@ $('.simple_form.edit_cardholder').on('submit',function(e){
   
 })
 
+$('input[type=file]').addClass('hidden').before('<a class="file_wrapper btn">Upload Photo</a><span class="file_wrapper_file_name"></span>').on('change',function(e){
+  e.stopPropagation();
+  e.preventDefault();
+
+  var files = e.currentTarget.files;
+
+  $(e.currentTarget).parent().find('.file_wrapper_file_name').text(" (Uploaded File: " + _.first(files).name + ")");
+});
+
+$('body').on('click','.file_wrapper',function(e){
+  $(e.currentTarget).parent().find('input[type=file]')[0].click();
+});
