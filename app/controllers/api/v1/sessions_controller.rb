edd386f60b68dfb3321dda9ee5222aa44e859825
Benjamin West <ben@quickleft.com>
2013-11-20 13:16:57 -0700

    removed pin authentication

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,16 +3,12 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     cardholder = Cardholder.find_by_phone_number(params[:phone_number])
-    if cardholder
-      if cardholder.pending?
-        render json: { auth_token: nil, onboarding: onboard_url(cardholder.onboarding_token) }
-      elsif cardholder.authenticate(params[:password])
-        render json: { auth_token: cardholder.auth_token }
-      else 
-        head :unauthorized
-      end
-    else
+    if cardholder.nil?
       head :unauthorized
+    elsif cardholder.pending?
+      render json: { auth_token: nil, onboarding: onboard_url(cardholder.onboarding_token) }
+    else
+      render json: { auth_token: cardholder.auth_token }
     end
   end
 end
