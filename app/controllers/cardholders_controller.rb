23c2cf7aa0c43527ac1331507c1c492363d9e07e
Brendan Benson <brendanbenson@gmail.com>
2014-06-26 22:45:24 -0600

    Search for cardholders by phone number
    [#64942724]

--- app/controllers/cardholders_controller.rb
+++ app/controllers/cardholders_controller.rb
@@ -27,7 +27,7 @@ class CardholdersController < ApplicationController
 
     if params[:filter].present?
       search_string = "%#{params[:filter]}%".downcase
-      @approved_cards = @approved_cards.where('lower(cardholders.last_name) LIKE ?', search_string)
+      @approved_cards = @approved_cards.where('lower(cardholders.last_name) LIKE ? OR cardholders.phone_number LIKE ?', search_string, search_string)
       @filter_string = params[:filter]
     end
     
